5b12935ca3a5cecf12538fc22694b58660166a68
Caio <caiow.wk@gmail.com>
2021-06-27 12:41:09 -0300

    feat(response-status): add response status code

--- src/main/kotlin/com/itidigital/validator/controller/v1/ValidatorController.kt
+++ src/main/kotlin/com/itidigital/validator/controller/v1/ValidatorController.kt
@@ -3,11 +3,9 @@ package com.itidigital.validator.controller.v1
 import com.itidigital.validator.domain.ValidatorResponse
 import com.itidigital.validator.dtos.ValidatePasswordRequest
 import com.itidigital.validator.service.PasswordValidatorService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
 
 @RestController
 @RequestMapping("api/v1")
@@ -16,6 +14,7 @@ class ValidatorController(
 ) {
 
     @PostMapping("/verify")
+    @ResponseStatus(HttpStatus.OK)
     fun validator(@RequestBody payload : ValidatePasswordRequest) : ValidatorResponse {
         return passowordValidatorService.validate(payload.password)
     }
